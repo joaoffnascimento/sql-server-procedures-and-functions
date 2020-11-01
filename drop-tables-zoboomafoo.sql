dee623a9906fd7455b3740816d22dd0da7a1cdd3
joaoffnascimento <thecurrentuser@live.com>
2020-10-31 22:59:24 -0300

    feat: add script drop tables

---
+++ drop-tables-zoboomafoo.sql
@@ -0,0 +1,11 @@
+DROP TABLE [Zoboomafoo].dbo.limpeza
+DROP TABLE [Zoboomafoo].dbo.historico
+DROP TABLE [Zoboomafoo].dbo.acompanha
+DROP TABLE [Zoboomafoo].dbo.animais
+DROP TABLE [Zoboomafoo].dbo.conteiner
+DROP TABLE [Zoboomafoo].dbo.funcionario	
+DROP TABLE [Zoboomafoo].dbo.enderecos
+DROP TABLE [Zoboomafoo].dbo.cidades
+DROP TABLE [Zoboomafoo].dbo.cargos
+DROP TABLE [Zoboomafoo].dbo.alas
+DROP TABLE [Zoboomafoo].dbo.classes
